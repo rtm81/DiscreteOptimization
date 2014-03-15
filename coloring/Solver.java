eeb2f9a6e6a66b849ed2280f0ab8c6bdcd5c8cad
rtm81 <christopher.roscoe@gmail.com>
2014-03-15 10:41:20 +0100

    38 / 60

--- coloring/Solver.java
+++ coloring/Solver.java
@@ -4,20 +4,27 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
 
+import com.google.common.collect.BiMap;
 import com.google.common.collect.Sets;
 import com.google.common.collect.Sets.SetView;
+import com.google.common.collect.TreeMultimap;
+import com.google.common.collect.TreeMultiset;
 
 
 public class Solver {
 
 	static boolean debug = false;
+	static boolean colorselectLargest = true;
 	
 	/**
 	 * The main class
@@ -50,31 +57,14 @@ public class Solver {
 
 		ProblemData problemData = getProblemDataFromFile(fileName);
 
-		class Edge {
-			int node1;
-			int node2;
-			public Edge(int i, int j) {
-				this.node1 = i;
-				this.node2 = j;
-			}
-			@Override
-			public String toString() {
-				return "Edge [" + node1 + ", " + node2 + "]";
-			}
-			
-		}
-		
-		class Node {
-			List<Edge> edges = new ArrayList<Edge>();
-			Integer color = null;
-			@Override
-			public String toString() {
-				return "Node [" + edges + ", color=" + color + "]";
-			}
-			
-		}
 		if (debug) {
-			System.out.println(Arrays.deepToString(problemData.getEdges()));
+			System.out.println("problem:\t" + Arrays.deepToString(problemData.getEdges()));
+		}
+		ColorSelector fromLargestSetColorSelector;
+		if (colorselectLargest) {
+			fromLargestSetColorSelector = new FromLargestSetColorSelector();
+		} else {
+			fromLargestSetColorSelector = new FirstColorSelector();
 		}
 		
 		Map<Integer, Node> nodes = new TreeMap<Integer, Node>();
@@ -84,27 +74,47 @@ public class Solver {
 			Edge edge = new Edge(nodeNr1, nodeNr2);
 			Node node1 = nodes.get(nodeNr1);
 			if (node1 == null) {
-				node1 = new Node();
+				node1 = new Node(nodeNr1);
 				nodes.put(nodeNr1, node1);
 			}
 			node1.edges.add(edge);
 			Node node2 = nodes.get(nodeNr2);
 			if (node2 == null) {
-				node2 = new Node();
+				node2 = new Node(nodeNr2);
 				nodes.put(nodeNr2, node2);
 			}
 			node2.edges.add(edge);
 		}
+		Comparator<? super Node> comparator = new Comparator<Node>() {
+
+			@Override
+			public int compare(Node o1, Node o2) {
+				return Integer.valueOf(o1.id).compareTo(Integer.valueOf(o1.id));
+			}
+		};
 		
-		if (debug) {
-			System.out.println(nodes);
-			System.out.println(nodes.size());
+		TreeMap<Integer, Set<Node>> rankSortedNodes = new TreeMap<Integer, Set<Node>>();
+		
+		for (Node node : nodes.values()) {
+			Integer rank = node.edges.size();
+			Set<Node> set = rankSortedNodes.get(rank);
+			if (set == null) {
+				set = new LinkedHashSet<Node>();
+				rankSortedNodes.put(rank, set);
+			}
+			set.add(node);
 		}
 		
-		Set<Integer> allColors = new HashSet<Integer>();
-		for (Entry<Integer, Node> entry : nodes.entrySet()) {
-			Integer nodeNr = entry.getKey();
-			Node node = entry.getValue();
+		
+		if (debug) {
+			System.out.println("nodes:\t" + nodes);
+			System.out.println("rank:\t" + rankSortedNodes);
+		}
+		
+		Map<Integer, Set<Node>> allColors = new LinkedHashMap<Integer, Set<Node>>();
+		for (Set<Node> rankNodes : rankSortedNodes.descendingMap().values()) {
+			for (Node node : rankNodes) {
+			
 			if (node.color != null) continue;
 			Set<Integer> neighborcolors = new HashSet<Integer>();
 			for (Edge edge : node.edges) {
@@ -112,21 +122,35 @@ public class Solver {
 				neighborcolors.add(nodes.get(edge.node2).color);
 			}
 			int possibleColor = 0;
-			SetView<Integer> difference = Sets.difference(allColors, neighborcolors);
+			SetView<Integer> difference = Sets.difference(allColors.keySet(), neighborcolors);
 			if (!difference.isEmpty()) {
-				possibleColor = difference.iterator().next();
+				possibleColor = fromLargestSetColorSelector.getColor(allColors, difference);
 			}
 			
 			while (neighborcolors.contains(possibleColor)) {
 				possibleColor++;
 			}
 			node.color = possibleColor;
-			allColors.add(possibleColor);
+			Set<Node> set = allColors.get(possibleColor);
+			if (set == null) {
+				set = new LinkedHashSet<Node>();
+				allColors.put(possibleColor, set);
+			}
+			set.add(node);
+			}
+		}
+		Map<Integer, Integer> colorsize = new LinkedHashMap<Integer, Integer>();
+		TreeMultimap<Integer,Integer> treeMultimap = TreeMultimap.create();
+		for (Entry<Integer, Set<Node>> entry : allColors.entrySet()) {
+			colorsize.put(entry.getKey(), entry.getValue().size());
+			treeMultimap.put(entry.getValue().size(), entry.getKey());
 		}
 
 		if (debug) {
-			System.out.println(nodes);
-			System.out.println(nodes.size());
+			System.out.println("nodes:\t" +nodes);
+			System.out.println("allColors:\t" +allColors);
+			System.out.println("colorsize:\t" +colorsize);
+			System.out.println("colorsize rev:\t" +treeMultimap);
 		}
 		
 		// prepare the solution in the specified output format
@@ -136,6 +160,41 @@ public class Solver {
 		}
 		System.out.println("");
 	}
+	
+	interface ColorSelector {
+		public int getColor(Map<Integer, Set<Node>> allColors,
+				SetView<Integer> difference);
+	}
+	
+	static class FirstColorSelector implements ColorSelector {
+
+		@Override
+		public int getColor(Map<Integer, Set<Node>> allColors,
+				SetView<Integer> difference) {
+			return difference.iterator().next();
+		}
+		
+	}
+	static class FromLargestSetColorSelector implements ColorSelector {
+		
+		public int getColor(Map<Integer, Set<Node>> allColors,
+				SetView<Integer> difference) {
+			int possibleColor;
+			int colorInlargestSet = -1;
+			int largestSet = -1;
+			for (Integer color : difference) {
+				int size = allColors.get(color).size();
+				if (size > largestSet) {
+					largestSet = size;
+					colorInlargestSet = color;
+				}
+			}
+			
+			possibleColor = colorInlargestSet;
+			return possibleColor;
+		}
+	}
+
 
 	public static ProblemData getProblemDataFromFile(String fileName)
 			throws FileNotFoundException, IOException {
@@ -170,4 +229,33 @@ public class Solver {
 		return problemData;
 	}
 
+	static class Edge {
+		int node1;
+		int node2;
+		public Edge(int i, int j) {
+			this.node1 = i;
+			this.node2 = j;
+		}
+		@Override
+		public String toString() {
+			return "Edge [" + node1 + ", " + node2 + "]";
+		}
+		
+	}
+	
+	static class Node {
+		private final int id;
+		List<Edge> edges = new ArrayList<Edge>();
+		Integer color = null;
+		
+		Node(final int id) {
+			this.id = id;
+		}
+		
+		@Override
+		public String toString() {
+			return "Node "+id+" [" + edges + ", color=" + color + "]";
+		}
+		
+	}
 }
